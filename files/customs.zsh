d753d5700611aa3201b09185644db5636f6999be
Max Wittmann <damxam@gmail.com>
2018-03-15 11:22:13 +1100

    Commit all the things done since dev broke

--- files/customs.zsh
+++ files/customs.zsh
@@ -72,6 +72,8 @@ alias kafkaoff='~/Workspaces/Libraries/Kafka/kafka_2.12-1.0.1/bin/kafka-server-s
 
 ## kubernetes
 alias deletePods='kubectl delete --all pods'
+alias minikubeon='minikube start --vm-driver none'
+alias minikubeoff='minikube stop'
 
 ## Npm / Frontend stuff
 alias npmi='npm install'
@@ -102,6 +104,6 @@ tnamef() {
 alias tname=tnamef
 
 #export PATH="/home/damxam/Workspaces/Libraries/anaconda3/bin:$PATH"
-export PATH="/home/damxam/Workspaces/Libraries/bin:/home/damxam/Workspaces/Libraries/Anaconda/bin:$PATH"
+export PATH="/home/damxam/Workspaces/Libraries/bin:/home/damxam/Workspaces/Tools/bin:/home/damxam/Workspaces/Libraries/Anaconda/bin:$PATH"
 export AIRFLOW_HOME=~/Workspaces/Libraries/airflow
 export JAVA_HOME="/usr/lib/jvm/java-8-oracle/"
