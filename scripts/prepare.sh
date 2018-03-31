4b9a4331909dbb974bb809ec3dab81c2f65b58c1
Max Wittmann <damxam@gmail.com>
2018-03-31 00:27:26 -0700

    Works?

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -1,2 +1,5 @@
-sudo apt update
+#sudo apt-add-repository ppa:ansible/ansible
+sudo apt-add-repository -y -k pgp.mit.edu ppa:ansible/ansible
+sudo apt-get update
 sudo apt install ansible git
+sudo apt-get install software-properties-common
