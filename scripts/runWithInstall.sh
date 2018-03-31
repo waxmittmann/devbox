b906cc8b4f521e7936ef7262012f9b4596fbe8cc
Max Wittmann <damxam@gmail.com>
2018-03-31 04:12:48 -0700

    Use localConfig.yml to override config.yml

--- scripts/runWithInstall.sh
+++
@@ -1,1 +0,0 @@
-sudo ansible-playbook --vault-password-file vault_pass.txt -i "localhost," -c local playbook.yml -vvvv --extra-vars "{ install_$1: true }"
--- scripts/runWithVars.sh
+++
@@ -1,1 +0,0 @@
-sudo ansible-playbook --vault-password-file vault_pass.txt -i "localhost," -c local playbook.yml -vvvv --extra-vars "$1"
