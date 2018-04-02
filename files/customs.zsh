031f5867fcc26f95e3bcbe18ef312643744e69bc
Max Wittmann <damxam@gmail.com>
2018-04-01 18:42:02 -0700

    Fix zsh template; make zsh role not depend on aptinstalls (else it runs it every time)

--- files/customs.zsh
+++ files/customs.zsh
@@ -2,7 +2,7 @@
 
 # Permanent Aliases
 ## devbox (Will need to be adjusted for workspace)
-alias runDevbox='{{ workspace_dir }}/Projects/Useful/devbox/run.sh'
+alias runDevbox='{{ install.workspace_dir }}/Projects/Useful/devbox/run.sh'
 
 ## docker
 function rmimgfn () { docker image rm -f $(docker images $1 -q) }
@@ -10,21 +10,21 @@ alias rmimg=rmimgfn
 
 ## Go places
 alias goh='cd ~'
-alias govnote='cd {{ workspace_dir }}/Notes/VersionedNotes'
-alias openvnote='atom {{ workspace_dir }}/Notes/VersionedNotes'
-alias gcvnote='{{ workspace_dir }}/Notes/VersionedNotes/std.sh'
-alias gonote='cd {{ workspace_dir }}/Notes'
+alias govnote='cd {{ install.workspace_dir }}/Notes/VersionedNotes'
+alias openvnote='atom {{ install.workspace_dir }}/Notes/VersionedNotes'
+alias gcvnote='{{ install.workspace_dir }}/Notes/VersionedNotes/std.sh'
+alias gonote='cd {{ install.workspace_dir }}/Notes'
 
-alias gows='cd {{ workspace_dir }}'
-alias gows='cd {{ workspace_dir }}'
-alias gotool='cd {{ workspace_dir }}/Tools'
-alias goide='cd {{ workspace_dir }}/IDEs'
-alias golib='cd {{ workspace_dir }}/Libraries'
+alias gows='cd {{ install.workspace_dir }}'
+alias gows='cd {{ install.workspace_dir }}'
+alias gotool='cd {{ install.workspace_dir }}/Tools'
+alias goide='cd {{ install.workspace_dir }}/IDEs'
+alias golib='cd {{ install.workspace_dir }}/Libraries'
 
-alias gop='cd {{ workspace_dir }}/Projects'
-alias gouse='cd {{ workspace_dir }}/Projects/Useful'
-alias goexp='cd {{ workspace_dir }}/Projects/Experimental'
-alias golearn='cd {{ workspace_dir }}/Projects/Learning'
+alias gop='cd {{ install.workspace_dir }}/Projects'
+alias gouse='cd {{ install.workspace_dir }}/Projects/Useful'
+alias goexp='cd {{ install.workspace_dir }}/Projects/Experimental'
+alias golearn='cd {{ install.workspace_dir }}/Projects/Learning'
 
 ## Look for java processes
 alias psj='ps -A | grep java'
@@ -32,14 +32,17 @@ alias psj='ps -A | grep java'
 ## Do stuff with zsh
 # Todo: this should change the devbox file and then copy that across
 alias srcsh='source ~/.zshrc'
-alias opensh='atom {{ workspace_dir }}/Projects/Useful/devbox/files/customs.zsh'
+alias opensh='atom {{ install.workspace_dir }}/Projects/Useful/devbox/files/customs.zsh'
 alias catsh='cat ~/.oh-my-zsh/custom/customs.zsh'
-alias gosh='cd {{ workspace_dir }}/Projects/Useful/devbox/files/customs.zsh'
+alias gosh='cd {{ install.workspace_dir }}/Projects/Useful/devbox/files/customs.zsh'
 alias savesh=''
 
 ## Github
 alias gitinfo='curl https://api.github.com -u $GITHUB_USER:$GITHUB_PASS'
 alias gitrli='curl https://api.github.com/user/repos -u $GITHUB_USER:$GITHUB_PASS'
+
+## Git extras
+alias gstd='gaa; gcmsg "No comment."; gp;'
 
 ## Sbt (TODO: Doesn't work)
 alias sbtopts='eval export SBT_OPTS="-Xmx2G -XX:+UseConcMarkSweepGC -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=2G -Xss2M  -Duser.timezone=GMT"'
@@ -81,8 +84,8 @@ alias sbtt='sbt -mem 3000'
 alias sbtr='sbt -mem 3000 "run local.conf"'
 
 ## Kafka stuff
-alias kafkaon='{{ workspace_dir }}/Libraries/Kafka/kafka_2.12-1.0.1/bin/kafka-server-start.sh {{ workspace_dir }}/Libraries/Kafka/kafka_2.12-1.0.1/config/server.properties &'
-alias kafkaoff='{{ workspace_dir }}/Libraries/Kafka/kafka_2.12-1.0.1/bin/kafka-server-stop.sh'
+alias kafkaon='{{ install.workspace_dir }}/Libraries/Kafka/kafka_2.12-1.0.1/bin/kafka-server-start.sh {{ install.workspace_dir }}/Libraries/Kafka/kafka_2.12-1.0.1/config/server.properties &'
+alias kafkaoff='{{ install.workspace_dir }}/Libraries/Kafka/kafka_2.12-1.0.1/bin/kafka-server-stop.sh'
 
 ## kubernetes
 alias deletePods='kubectl delete --all pods'
@@ -108,10 +111,10 @@ function gcheatfn () { catcheat | grep $1 }
 alias gcheat=gcheatfn
 
 # Temporary links, review occasionally
-alias secrets='eval $({{ workspace_dir }}/Secrets/secret_env.sh)'
+alias secrets='eval $({{ install.workspace_dir }}/Secrets/secret_env.sh)'
 
 ## Tensorflow
-alias tensoron='source {{ workspace_dir }}/Libraries/tensorflow/bin/activate'
+alias tensoron='source {{ install.workspace_dir }}/Libraries/tensorflow/bin/activate'
 
 ## This does not work as is (https://stackoverflow.com/questions/5343265/setting-title-for-tabs-in-terminator-console-application-in-ubuntu)
 tnamef() {
@@ -122,6 +125,6 @@ tnamef() {
 alias tname=tnamef
 
 #export PATH="/home/damxam/Workspaces/Libraries/anaconda3/bin:$PATH"
-export PATH="{{ workspace_dir }}/Libraries/bin:{{ workspace_dir }}/Tools/bin:{{ workspace_dir }}/Libraries/Anaconda/bin:$PATH"
-export AIRFLOW_HOME="{{ workspace_dir }}/Libraries/airflow"
+export PATH="{{ install.workspace_dir }}/Libraries/bin:{{ install.workspace_dir }}/Tools/bin:{{ install.workspace_dir }}/Libraries/Anaconda/bin:$PATH"
+export AIRFLOW_HOME="{{ install.workspace_dir }}/Libraries/airflow"
 export JAVA_HOME="/usr/lib/jvm/java-8-oracle/"
