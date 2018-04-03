8fc311f3ef3a178feb3da2d2fd093a3827eb8f9e
Max Wittmann <damxam@gmail.com>
2018-04-04 09:11:59 +1000

    Check version of ansible on startup

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -1,5 +1,6 @@
 #sudo apt-add-repository ppa:ansible/ansible
-sudo apt-add-repository -y -k pgp.mit.edu ppa:ansible/ansible
+#sudo apt-add-repository -y -k pgp.mit.edu ppa:ansible/ansible
+sudo apt-add-repository -y ppa:ansible/ansible
 sudo apt-get update
 sudo apt install ansible git
 sudo apt-get install software-properties-common
